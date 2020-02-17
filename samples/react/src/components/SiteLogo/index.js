681b50615f7353d8e1437fc0a45e7e364e35b6a7
Fredy Alvarado, Jr <falvarado@degdigital.com>
2020-02-17 15:58:23 -0600

    added default text and removed inline style

--- samples/react/src/components/SiteLogo/index.js
+++ samples/react/src/components/SiteLogo/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import logo from '../../images/logo.svg';
 
-const SiteLogo = () => {
+const SiteLogo = ({ logoTitle }) => {
   return (
     <a href="/" className="logo">
-      <img
-        src={logo}
-        alt=""
-        style={{
-          width: '160px',
-        }}
-      />
+      <img src={logo} alt="" />
+      {logoTitle}
     </a>
   );
+};
+
+SiteLogo.propTypes = {
+  logoTitle: PropTypes.string,
+};
+
+SiteLogo.defaultProps = {
+  logoTitle: 'Company Logo',
 };
 
 export default SiteLogo;
